46f113113f33ebc7fb5bafdea1f2eee8ba9925ef
kwiecieno <ok7082385@gmail.com>
2022-01-08 16:31:13 +0100

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function reverse (n) {
-  
+  var stringOfN = String(n);
+  var i = 0;
+  var result = '';
+if (n < 0 ) {
+  var positiveN = n * (-1);
+  var stringOfPositiveN = String(positiveN);
+  while (i < stringOfPositiveN.length) {
+    result = stringOfPositiveN[i] + result;
+    i++;
+  }
 }
+  else {
+    while (i < stringOfN.length) {
+        result = stringOfN[i] + result;
+        i++;
+      }
+
+  }
+
+  return Number(result);
+};
